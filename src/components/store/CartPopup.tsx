22737a9716c89d339ac653b1620e666b82516270
Daniel Ilies <iliesh.daniel@gmail.com>
2024-04-04 15:30:43 +0400

    cartitem added

--- src/components/store/CartPopup.tsx
+++ src/components/store/CartPopup.tsx
@@ -1,9 +1,29 @@
+import { Avatar, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Stack, Typography } from "@mui/material"
+import { Product } from "./ProductCard"
+import { Delete } from "@mui/icons-material"
 
-const CartPopup = () => {
+const CartPopup = ({ item }: { item: Product }) => {
   return (
-    <>
-      Hello world!
-    </>
+    <Stack>
+      <List>
+        <ListItem sx={{ width: 300 }} alignItems="center">
+          <ListItemAvatar>
+            <Avatar src={`https://picsum.photos/50`} />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Product here"
+            secondary={
+              <Typography variant="caption">
+                12 x 3
+              </Typography>
+            }
+          />
+          <ListItemButton>
+            <Delete />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Stack>
   )
 }
 
